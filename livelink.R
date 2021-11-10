d0bc713d13a097c3a53e37ee85bfe719f6261642
Eugene Paulia <ypaulia@suncor.com>
2021-11-10 15:26:48 -0700

    extracted livelink data pull code into a separate r file, and sourcing it now into the main "covid-leadership  - trycatch" script

---
+++ livelink.R
@@ -0,0 +1,24 @@
+download.file(
+  url = "http://livelink/ecm/nodes/709040692/Essential%20Workers%20List.xlsx",
+  destfile="C:/Users/ypaulia/Documents/Covid-Leadership trycatch dev/Test Files Dump/Essential Workers List.xlsx",
+  mode="wb"
+)
+
+download.file(
+  url = "http://livelink/ecm/nodes/709040692/Headcount%20for%20EW.xlsx",
+  destfile="C:/Users/ypaulia/Documents/Covid-Leadership trycatch dev/Test Files Dump/Headcount for EW.xlsx",
+  mode="wb"
+)
+
+download.file(
+  url = "http://livelink/ecm/nodes/709040692/Work%20Addresses.xlsx",
+  destfile="C:/Users/ypaulia/Documents/Covid-Leadership trycatch dev/Test Files Dump/Work Addresses.xlsx",
+  mode="wb"
+)
+
+# AKT_IDM
+download.file(
+  url = "http://livelink/ecm/nodes/709040692/AKT_IDM.xlsx",
+  destfile="C:/Users/ypaulia/Documents/Covid-Leadership trycatch dev/Test Files Dump/AKT_IDM.xlsx",
+  mode="wb"
+)
