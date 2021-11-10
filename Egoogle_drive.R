cac47df71cf311acec5634721992a4416f065f9e
Eugene Paulia <ypaulia@suncor.com>
2021-11-10 14:21:38 -0700

    Extracted google drive function into a new R script and sourcing this script now into the main R script - covid-leadership - trycatch

---
+++ Egoogle_drive.R
@@ -0,0 +1,6 @@
+remDr$navigate('https://drive.google.com/file/d/1cjLn3Qkvs8hNJDGzCuqrDzbeNechUmkW/view')
+
+remDr$findElement(
+  using = 'xpath',
+  value = '//div[@class = "ndfHFb-c4YZDc-Bz112c ndfHFb-c4YZDc-C7uZwb-LgbsSe-Bz112c ndfHFb-c4YZDc-nupQLb-Bz112c"]'
+)$clickElement()
